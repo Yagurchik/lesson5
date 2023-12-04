04b429dcb9a6baff5d8d4a6dd204969fb283ef7c
LAPTOP-58VVCJPI\greds <gredseven@mail.ru>
2023-12-05 00:49:47 +0300

    Homework 5, ex.2

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace HomeWork5
+{
+    internal class Program
+    {
+        static void Homework5(string[] args)
+        {
+            if (uint.TryParse(Console.ReadLine(), out uint FNumber) && uint.TryParse(Console.ReadLine(), out uint SNumber))
+            {
+                for (int mnoj = 1; mnoj <= SNumber; mnoj++) 
+                { 
+                Console.WriteLine(FNumber + " x " + mnoj + " = " + (FNumber*mnoj));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
