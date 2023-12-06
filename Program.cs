2ba3eab806297c727edd05bfb5666198de0fbce6
Yagurchik <152524187+Yagurchik@users.noreply.github.com>
2023-12-07 02:13:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HW4_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Укажите ваш доход:");
+            if (double.TryParse(Console.ReadLine(), out double result))
+            {
+                double tax = result * 0.13;
+                Console.WriteLine("Налог для уплаты в конце месяца составит: " + tax);
+            }
+            else
+            {
+                Console.WriteLine("неверные данные");
+            }
+        }
+    }
+}
